bd58dcf3969fe07ad9f474d25c52174ed4c5e63f
mdbug <michael_vb@web.de>
2016-10-20 17:10:10 +0200

    Add files via upload

---
+++ blatt02/EMailAdressBuch.java
@@ -0,0 +1,21 @@
+
+public class EMailAdressBuch {
+
+	public EMailAdressBuch() {
+		//TODO
+	}
+	
+	public void einfuegen(String name, String email) {
+		//TODO
+	}
+	
+	public String abfrage(String name) {
+		//TODO
+		return null;
+	}
+	
+	public String toString() {
+		//TODO
+		return null;
+	}
+}
