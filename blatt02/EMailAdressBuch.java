7a5c2deabfb5542a829c0b5b7ab62d318c9b6b24
AndiBr97 <andi_br@gmx.net>
2016-10-21 14:17:38 +0200

    Update EMailAdressBuch.java
    Implementierung von toString() und einlesen() (lokale Datei)

--- blatt02/EMailAdressBuch.java
+++ blatt02/EMailAdressBuch.java
@@ -1,43 +1,69 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class EMailAdressBuch {
-	private HashMap<String, String> map;
+    private HashMap<String, String> map = new HashMap<>();
 
-	/**
-	 * Konstruiert ein neues EMailAddressBuch-Objekt
-	 */
-	public EMailAdressBuch() {
-		map = new HashMap<>();
-	}
+    /**
+     * Konstruiert ein neues EMailAddressBuch-Objekt
+     */
+    public EMailAdressBuch() {
+        map = new HashMap<>();
+    }
 
-	/**
-	 * Fügt einen neuen Kontakt zum Adressbuch hinzu
-	 * oder aktuallisiert die E-Mail-Adresse eines Kontakts.
-	 * @param name der Name des Kontakts
-	 * @param email die Email Adresse des Kontakts
-	 */
-	public void einfuegen(String name, String email) {
-		map.put(name, email);
-	}
-	
-	/**
-	 * Liefert die E-Mail-Adresse fuer einen gegebenen Namen zurueck
-	 * @param name der Name des Kontakts
-	 * @return die Emailadresse des Kontakts
-	 * @throws UnknownNameException falls der Name nicht im Adressbuch vorhanden ist
-	 */
-	public String abfrage(String name) {
-		if (!map.containsKey(name))
-			throw new UnknownNameException("Name " + name+ " nicht gefunden");
+    /**
+     * FÃ¼gt einen neuen Kontakt zum Adressbuch hinzu
+     * oder aktuallisiert die E-Mail-Adresse eines Kontakts.
+     * @param name der Name des Kontakts
+     * @param email die Email Adresse des Kontakts
+     */
+    public void einfuegen(String name, String email) {
+        map.put(name, email);
+    }
 
-		return map.get(name);
-	}
+    /**
+     * Liefert die E-Mail-Adresse fuer einen gegebenen Namen zurueck
+     * @param name der Name des Kontakts
+     * @return die Emailadresse des Kontakts
+     * @throws UnknownNameException falls der Name nicht im Adressbuch vorhanden ist
+     */
+    public String abfrage(String name) {
+        if (!map.containsKey(name))
+            throw new UnknownNameException("Name " + name+ " nicht gefunden");
 
-	/**
-	 * 
-	 */
-	public String toString() {
-		// TODO
-		return null;
-	}
+        return map.get(name);
+    }
+
+    /**
+     * Liefert einen String zurueck, der saemtliche Namen mit entsprechender E-Mail enthaelt
+     * @return Namen und EMails
+     */
+    public String toString() {
+        StringBuilder builder = new StringBuilder("{");
+        for(String s: map.keySet()){
+            String vorname = s.split(" ",2)[0];
+            if(s.equals(map.keySet().toArray()[map.keySet().size()-1])) builder.append(vorname+"}");
+            else builder.append(vorname+"="+abfrage(s)+", ");
+        }
+        return builder.toString();
+    }
+
+    /**
+     *  Liest Emails von einer lokalen Datei
+     * @param dateiname Name der Datei
+     * @throws FileNotFoundException falls die Datei nicht vorhanden ist
+     */
+    public void einlesen(String dateiname) throws FileNotFoundException {
+        File f = new File(dateiname);
+        Scanner sc = new Scanner(f);
+
+        while(sc.hasNextLine()){
+            String current=sc.nextLine();
+            String[] temp = current.split(";");
+            einfuegen(temp[0],temp[1]);
+        }
+        sc.close();
+    }
 }
