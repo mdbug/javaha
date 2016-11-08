5279344ee4ac8a52010f8af9a99b8998fd088b0c
Michael von Bothmer <michael_vb@web.de>
2016-11-08 21:51:41 +0100

    ADD: JavaDoc

--- blatt04/PositiveNumber.java
+++ blatt04/PositiveNumber.java
@@ -1,28 +1,59 @@
-//TODO JavaDoc
+/**
+ * @author Michael von Bothmer
+ * @author Andreas Braining
+ * @author Richard Deterer
+ */
 public class PositiveNumber {
-    private int value;
     private final static String DIGITS = "0123456789ABCDEF";
+    private int value;
 
+    /**
+     * Die als String uebergebene Dezimalzahl wird gesetzt
+     * @param s eine Dezimalzahl als String
+     * @throws NumberFormatException falls der String keine gueltige Dezimalzahl repraesentiert
+     */
     public void setDecimal(String s) throws NumberFormatException {
         setValue(s, 10);
     }
 
+    /**
+     * Die als String uebergebene Hexadezimalzahl wird gesetzt
+     * @param s eine Hexadezimalzahl als String
+     * @throws NumberFormatException falls der String keine gueltige Hexadezimalzahl repraesentiert
+     */
     public void setHexadecimal(String s) throws NumberFormatException {
         setValue(s, 16);
     }
 
+    /**
+     * Die als String uebergebene Binaerzahl wird gesetzt
+     * @param s eine Binaerzahl als String
+     * @throws NumberFormatException falls der String keine gueltige Binaerzahl repraesentiert
+     */
     public void setBinary(String s) throws NumberFormatException {
         setValue(s, 2);
     }
 
+    /**
+     * Gibt die Zahl im Dezimalsystem als String zurueck
+     * @return die Zahl im Dezimalsystem als String 
+     */
     public String getDecimal() {
         return getValue(10);
     }
 
+    /**
+     * Gibt die Zahl im Hexadezimalsystem als String zurueck
+     * @return die Zahl im Hexadezimalsystem als String 
+     */
     public String getHexadecimal() {
         return getValue(16);
     }
 
+    /**
+     * Gibt die Zahl im Binaersystem als String zurueck
+     * @return die Zahl im Binaersystem als String 
+     */
     public String getBinary() {
         return getValue(2);
     }
@@ -30,8 +61,8 @@ public class PositiveNumber {
     private void setValue(String s, int radix) throws NumberFormatException, ArithmeticException {
         s = s.toUpperCase();
         int value = 0;
-        for (int i = 0, factor = 1; i < s.length(); i++, factor*=radix) {
-            char c = s.charAt(s.length()-1-i);
+        for (int i = s.length()-1, factor = 1; i >= 0; i--, factor*=radix) {
+            char c = s.charAt(i);
             int digit = DIGITS.indexOf(c);
             if (digit == -1 || digit >= radix) {
                 throw new NumberFormatException("Bad Character: " + c);
